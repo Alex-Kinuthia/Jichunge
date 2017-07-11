7f852e97451620892eaa28b79778fc40af5283a0
Collins Mwiti <mwiticmutumamwenda@gmail.com>
2017-07-11 18:40:31 +0300

    made settings to be active where users can change their settings without moving out of the app

--- app/src/main/java/com/example/alex/jichunge/ui/MainActivity.java
+++ app/src/main/java/com/example/alex/jichunge/ui/MainActivity.java
@@ -102,6 +102,8 @@ public class MainActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent settings = new Intent(android.provider.Settings.ACTION_SETTINGS);
+            startActivity(settings);
             return true;
         }
 
