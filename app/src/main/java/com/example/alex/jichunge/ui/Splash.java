9250aa9a8adf6855750c05f979a4cbac774aea65
Alex-Kinuthia <alexkinuthia125@gmail.com>
2017-07-12 15:19:39 +0300

    dealing with user authentification

--- app/src/main/java/com/example/alex/jichunge/ui/Splash.java
+++ app/src/main/java/com/example/alex/jichunge/ui/Splash.java
@@ -47,7 +47,7 @@ public class Splash extends AppCompatActivity {
             public void onAnimationEnd(Animation animation) {
                 imageView.startAnimation(animation2);
                 finish();
-                Intent intent = new Intent(getBaseContext(), SlidingClass.class);
+                Intent intent = new Intent(getBaseContext(), LoginActivity.class);
                 startActivity(intent);
             }
 
